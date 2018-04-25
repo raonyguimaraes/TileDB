2b0a3963dd7d3371d3899c10bfb4f73333048e0e
Tyler Denniston <tyler@tiledb.io>
2018-04-25 17:43:46 -0400

    Hide globalState instance

--- tiledb/sm/storage_manager/storage_manager.cc
+++ tiledb/sm/storage_manager/storage_manager.cc
@@ -64,7 +64,7 @@ StorageManager::StorageManager() {
 }
 
 StorageManager::~StorageManager() {
-  global_state::globalState.unregister_storage_manager(this);
+  global_state::GlobalState::GetGlobalState().unregister_storage_manager(this);
 
   async_stop();
   delete async_thread_;
@@ -553,7 +553,7 @@ Status StorageManager::init(Config* config) {
   vfs_ = new VFS();
   RETURN_NOT_OK(vfs_->init(config_.vfs_params()));
 
-  global_state::globalState.register_storage_manager(this);
+  global_state::GlobalState::GetGlobalState().register_storage_manager(this);
 
   return Status::Ok();
 }
